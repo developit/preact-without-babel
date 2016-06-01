4e35697106dcb07162497b355265bfc955319b0a
Linus Unnebäck <linus@folkdatorn.se>
2016-06-01 18:15:29 +0200

    Upgrade the code base to ES2015
    Fixes #2

--- index.js
+++ index.js
@@ -1,34 +1,14 @@
-// alias preact's hyperscript reviver since it's referenced a lot:
-var h = preact.h;
+'use strict';
 
-
-// To use Classful Components in ES3/5, use your favorite inheritance technique.
-// If you don't intend to use the Component class, you can skip this.
-// Here's an example:
-function createClass(obj) {
-	// sub-class Component:
-	function F(){ preact.Component.call(this); }
-	var p = F.prototype = Object.create(preact.Component.prototype);
-	// copy our skeleton into the prototype:
-	for (var i in obj) {
-            if (i === 'getDefaultProps' && typeof obj.getDefaultProps === 'function') {
-                F.defaultProps = obj.getDefaultProps() || {};
-            } else {
-                p[i] = obj[i];
-            }
-        }
-	// restore constructor:
-	return p.constructor = F;
-}
-
+const { Component, h, render } = window.preact;
 
 
 /** Example classful component */
-var App = createClass({
-	componentDidMount: function() {
+class App extends Component {
+	componentDidMount() {
 		this.setState({ message:'Hello!' });
-	},
-	render: function(props, state) {
+	}
+	render(props, state) {
 		return (
 			h('div', {id:'app'},
 				h(Header, { message: state.message }),
@@ -36,22 +16,22 @@ var App = createClass({
 			)
 		);
 	}
-});
+}
 
 
 /** Components can just be pure functions */
-var Header = function(props) {
+const Header = (props) => {
 	return h('header', null,
 		h('h1', null, 'App'),
-		props.message ? h('h2',null,props.message) : null
+		props.message ? h('h2', null, props.message) : null
 	);
 }
 
 
 /** Instead of JSX, use: h(type, props, ...children) */
-var Main = createClass({
-	render: function() {
-		var items = [1,2,3,4,5].map( function(item) {
+class Main extends Component {
+	render() {
+		const items = [1,2,3,4,5].map((item) => {
 			return h('li', {id:item}, 'Item '+item);
 		});
 		return (
@@ -60,7 +40,7 @@ var Main = createClass({
 			)
 		);
 	}
-});
+}
 
 
-preact.render(h(App), document.body);
+render(h(App), document.body);
